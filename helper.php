80cc97c14ef6d586be9b771515a2a949b8392663
Redknife <redknife183@gmail.com>
2014-06-25 17:18:09 +0600

    add all files

---
+++ helper.php
@@ -0,0 +1,26 @@
+<?php
+defined('_JEXEC') or die;
+
+class ReviewsHelper {
+	
+    public function generate_code() 
+    {    
+      $chars = 'qwertyuiopasdfghjklzxcvbnm23456789'; // Задаем символы, используемые в капче. Разделитель использовать не надо.
+      $length = rand(6, 7); // Задаем длину капчи, в нашем случае - от 4 до 7
+      $numChars = strlen($chars); // Узнаем, сколько у нас задано символов
+      $str = '';
+      for ($i = 0; $i < $length; $i++) {
+        $str .= substr($chars, rand(1, $numChars) - 1, 1);
+      } // Генерируем код
+
+      // Перемешиваем, на всякий случай
+      $array_mix = preg_split('//', $str, -1, PREG_SPLIT_NO_EMPTY);
+      srand ((float)microtime()*1000000);
+      shuffle ($array_mix);
+      // Возвращаем полученный код
+      return implode("", $array_mix);
+    }
+	
+}
+
+
